4ccfd8c99c173d12ce627ef667465e93f4d86e15
Falchio <53504233+Falchio@users.noreply.github.com>
2020-02-12 21:55:59 +0300

    Added locations list, button for change activity

--- app/src/main/java/ru/skillsnet/falchio/SelectCity.java
+++ app/src/main/java/ru/skillsnet/falchio/SelectCity.java
@@ -2,7 +2,15 @@ package ru.skillsnet.falchio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SelectCity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class SelectCity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_city);
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.enterUserLocation);
+        String[] locations = getResources().getStringArray(R.array.locations);
+        List<String> locationsList = Arrays.asList(locations);
+        ArrayAdapter<String> adapterLocations = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, locationsList);
+        autoCompleteTextView.setAdapter(adapterLocations);
+
+        Button toMainScreenButton = findViewById(R.id.toMainScreenButton);
+        toMainScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMainScreenIntent = new Intent(SelectCity.this, MainActivity.class);
+                startActivity(toMainScreenIntent);
+            }
+        });
     }
 }
