3641fd6fb14d0fffb2985f43734c2bdba3b0a149
Falchio <53504233+Falchio@users.noreply.github.com>
2020-02-16 15:51:12 +0300

    Added Toast and Log.D

--- app/src/main/java/ru/skillsnet/falchio/SelectCity.java
+++ app/src/main/java/ru/skillsnet/falchio/SelectCity.java
@@ -1,18 +1,24 @@
 package ru.skillsnet.falchio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class SelectCity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+    private final boolean DEBUG = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +39,91 @@ public class SelectCity extends AppCompatActivity {
                 startActivity(toMainScreenIntent);
             }
         });
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onCreate", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onCreate");
+        }
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onStart", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onStart");
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onRestoreInstanceState");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onResume", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onResume");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onPause", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onPause");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onSaveInstanceState", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onSaveInstanceState");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onStop", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onStop");
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onRestart", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onRestart");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onStop", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onStop");
+        }
+    }
+
 }
--- app/src/main/java/ru/skillsnet/falchio/SettingsActivity.java
+++ app/src/main/java/ru/skillsnet/falchio/SettingsActivity.java
@@ -1,12 +1,17 @@
 package ru.skillsnet.falchio;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+    private final boolean DEBUG = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,90 @@ public class SettingsActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onCreate", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onCreate");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onStart", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onStart");
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onRestoreInstanceState");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onResume", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onResume");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onPause", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onPause");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onSaveInstanceState", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onSaveInstanceState");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onStop", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onStop");
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onRestart", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onRestart");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (DEBUG){
+            Toast.makeText(getApplicationContext(),TAG +" onStop", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, TAG +" onStop");
         }
     }
 
